0f9ae4106ffe069b7c099574385a23e8f62df2fd
Borja <borjamese@gmail.com>
2023-03-07 11:13:44 +0000

    added changes

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function getRandomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // Select random elements from each array
+  let who = getRandomElement([
+    "The dog",
+    "My grandma",
+    "His turtle",
+    "My bird"
+  ]);
+  let action = getRandomElement(["ate", "peed", "crushed", "broke"]);
+  let what = getRandomElement(["my homework", "the keys", "the car"]);
+  let when = getRandomElement([
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ]);
+  let excuse = who + " " + action + " " + what + " " + when + ".";
+  let excuseDisplay = document.getElementById("excusa");
+  excuseDisplay.innerText = excuse;
 };
